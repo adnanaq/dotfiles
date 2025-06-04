b77d6521b5938fc034d33bf4808b866610fe9949
adnanaq <adnan_aq@hotmail.com>
2025-06-04 12:53:15 -0400

    Fix avante plugin and add anthropic api

--- .config/nvim/lua/plugins/avante-nvim.lua
+++ .config/nvim/lua/plugins/avante-nvim.lua
@@ -1,17 +1,28 @@
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
-	version = false, -- Never set this value to "*"! Never!
+	version = false,
+
 	opts = {
-		provider = "openai",
+		-- provider = "openai",
 		providers = {
 			openai = {
-				model = "gpt-4o",
+				model = "gpt-4.1", -- chat: gpt-4.1, Reasoning: o4-mini, o3
+			},
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-sonnet-4-20250514", -- Chat: Sonnet, Reasoning: Opus
+				extra_request_body = {
+					temperature = 0.75,
+					max_tokens = 4096,
+				},
 			},
 		},
 	},
+
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
+
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"stevearc/dressing.nvim",
@@ -22,6 +33,7 @@ return {
 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+
 		{
 			-- support for image pasting
 			"HakonHarnes/img-clip.nvim",
@@ -39,6 +51,7 @@ return {
 				},
 			},
 		},
+
 		{
 			-- Make sure to set this up properly if you have lazy=true
 			"MeanderingProgrammer/render-markdown.nvim",
